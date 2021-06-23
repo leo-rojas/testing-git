114fe48aae5c97c4369bd4fdb02d6d18d5567290
leo-rojas <leo-rojas@users.noreply.github.com>
2021-06-23 14:51:02 +0000

    Apply fixes from StyleCI

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -1,26 +1,23 @@
 <?php
-Class Admin_model extends CI_Model{		
-    
-    
-    function saveAdmin($update){
-        if(isset($update['id'])){
+
+class Admin_model extends CI_Model
+{
+    public function saveAdmin($update)
+    {
+        if (isset($update['id'])) {
             $this->db->update('admin', $update, array('id' => $update['id']));
             return $this->db->affected_rows();
-            
         } else {
             $this->db->insert('user', $update);
             return $this->db->insert_id();
         }
     }
-    
-    
-    function deleteAdmin($id){
+
+
+    public function deleteAdmin($id)
+    {
         $this->db->delete('admin', array('id' => $id));
-        
+
         return $this->db->affected_rows();
     }
-    
-    
-    
-    
 }
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,25 +1,23 @@
 <?php
-Class User_model extends CI_Model{		
-    
-    
-    function saveUser($update){
-        if(isset($update['id'])){
+
+class User_model extends CI_Model
+{
+    public function saveUser($update)
+    {
+        if (isset($update['id'])) {
             $this->db->update('user', $update, array('id' => $update['id']));
             return $this->db->affected_rows();
-            
         } else {
             $this->db->insert('user', $update);
             return $this->db->insert_id();
         }
     }
-    
-    
-    function deleteUserInfoTest($id){
+
+
+    public function deleteUserInfoTest($id)
+    {
         $this->db->delete('admin', array('id' => $id));
-        
+
         return $this->db->affected_rows();
     }
-    
-    
-    
 }
